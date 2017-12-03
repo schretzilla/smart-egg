d0793f908a0396cdf71a77c7d83513c68bc1b49f
schretzilla <schretzilla@gmail.com>
2017-12-02 16:47:06 -0800

    simple stock chart implemented as sample

--- dashboard/highchart/charting.js
+++ dashboard/highchart/charting.js
@@ -5,32 +5,47 @@ Highcharts.setOptions({
   }
 });
 
-// Create the chart
+var chart; //global chart
 Highcharts.stockChart('container', {
-  chart: {
-      events: {
-          load: function () {
+    chart: {
+        renderTo: 'container',
+        defaultSeriesType: 'spline',
+        events: {
+            load: function () {
 
-              // set up the updating of the chart each second
-              var series = this.series[0];
-              setInterval(function () {
-                  var x = (new Date()).getTime(), // current time
-                      y = Math.round(Math.random() * 100);
-                  series.addPoint([x, y], true, true);
-              }, 1000);
-          }
-      }
-  },
-
-  rangeSelector: {
+                // set up the updating of the chart each second
+                var series = this.series[0];
+                setInterval(function () {
+                    var x = (new Date()).getTime(), // current time
+                        y = Math.round(Math.random() * 100);
+                    series.addPoint([x, y], true, true);
+                    // console.log(series);                    
+                }, 1000);
+            }
+        }
+    },
+    title: {
+        text: 'Live random data'
+    },
+    xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150,
+        maxZoom: 20 * 1000,
+        range: 100
+    },
+    yAxis: {
+        minPadding: 0.2,
+        maxPadding: 0.2,
+        title: {
+            text: 'Value',
+            margin: 30
+        }
+    },
+    rangeSelector: {
       buttons: [{
           count: 1,
           type: 'minute',
           text: '1M'
-      }, {
-          count: 5,
-          type: 'minute',
-          text: '5M'
       }, {
           type: 'all',
           text: 'All'
@@ -38,30 +53,42 @@ Highcharts.stockChart('container', {
       inputEnabled: false,
       selected: 0
   },
-
-  title: {
-      text: 'Live random data'
-  },
-
   exporting: {
-      enabled: false
-  },
+          enabled: false
+      },
+    series: [{
+        name: 'Random data',
+        data: (function () {
+              // generate an array of random data
+              var data = [],
+                  time = (new Date()).getTime(),
+                  i;
+    
+              for (i = -999; i <= 0; i += 1) {
+                  data.push([
+                      time + i * 1000,
+                      Math.round(Math.random() * 100)
+                  ]);
+              }
+              return data;
+          }())
+        }]
+  });        
 
-  series: [{
-      name: 'Random data',
-      data: (function () {
-          // generate an array of random data
-          var data = [],
-              time = (new Date()).getTime(),
-              i;
 
-          for (i = -999; i <= 0; i += 1) {
-              data.push([
-                  time + i * 1000,
-                  Math.round(Math.random() * 100)
-              ]);
-          }
-          return data;
-      }())
-  }]
-});
+// Create the chart
+// Highcharts.stockChart('container', {
+//   chart: {
+//       events: {
+//           load: function () {
+
+//               // set up the updating of the chart each second
+//               var series = this.series[0];
+//               setInterval(function () {
+//                   var x = (new Date()).getTime(), // current time
+//                       y = Math.round(Math.random() * 100);
+//                   series.addPoint([x, y], true, true);
+//               }, 1000);
+//           }
+//       }
+//   },
