68dd11857731e938d66baa5a808ad2215308ec0e
Scott Schretzenmaier <schretzilla@gmail.com>
2017-12-05 20:36:59 -0800

    random data added on start at 10 points per turn. Mising scrollbar

--- dashboard/highchart/charting.js
+++ dashboard/highchart/charting.js
@@ -6,7 +6,7 @@ Highcharts.setOptions({
 });
 
 var chart; //global chart
-var refreshIntervalId; // used to stop recording
+var dataStreamInterval; // used to stop recording
 $("#stop").hide();
 
 // Stop recording and clear the chart
@@ -22,12 +22,8 @@ function stop() {
     $("#record").show();
     $("#stop").hide();
 
-    //Stop the recrodign interval
-    clearInterval(refreshIntervalId);   
-    
-    //Pull last data and display chart
-    testData = [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-    buildChart(testData);
+    //Stop the recoding interval
+    clearInterval(dataStreamInterval);   
 }
 
 // Start recording random data to chart
@@ -37,11 +33,41 @@ function record() {
     $("#stop").show();
 
     //Start pulling live data
-
+    let streamIndex = 0; // Index of last number added to chart
+    let dataList = [];
+    let chart = buildChart(dataList); // build empty chart initially
+    let timeBetweenDataPull = 1000;
+    dataStreamInterval = setInterval(function() {
+        let randomDataList = getRandomNumbersToAdd();
+        dataList = dataList.concat(randomDataList); // append new data (simulates the addition of data to the stream)
+        streamIndex = addPointsToChart(chart, dataList, streamIndex);  // move the cur index to include the newly aded values        
+    }, timeBetweenDataPull);
+        
 }         
 
+function streamData() {
+
+}
+
+function addPointsToChart(chart, dataToAdd, curIndex){
+    for(curIndex; curIndex < dataToAdd.length; curIndex++){
+        let curValue = dataToAdd[curIndex];
+        chart.series[0].addPoint(curValue);
+    }
+    return curIndex;
+}
+
+function getRandomNumbersToAdd(){
+    let randomDataList = [];
+    for(let i=0; i<10; i++){
+        let randNumber = Math.floor(Math.random() * 100);
+        randomDataList.push(randNumber);
+    }
+    return randomDataList;
+}
+
 function buildChart(resultData) {
-    Highcharts.chart('container', {
+    var chart = Highcharts.chart('container', {
             chart: {
                 zoomType: 'x'
             },
@@ -52,7 +78,10 @@ function buildChart(resultData) {
             subtitle: {
                 text: 'Team: [Team Name]'
             },
-        
+            xAxis: {
+                min:0,
+                max:100
+            },
             yAxis: {
                 title: {
                     text: 'Acceleration'
@@ -94,4 +123,6 @@ function buildChart(resultData) {
             }]
         }
     });
+
+    return chart;
 }
